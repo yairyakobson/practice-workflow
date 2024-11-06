4d84254bfd0e329c57b0c43b1f5fb25e986fc048
yairyakobson <yairnet92@gmail.com>
2024-11-06 10:35:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import eventRoutes from "./routes/events.js";
+
+const app = express();
+app.express(json());
+
+app.use(eventRoutes);
+
+app.listen(process.env.PORT);
---
+++ routes/events.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import db from "../data/database.js";
+
+const router = Router();
+
+router.get("/", async (req, res) =>{
+  const allEvents = await db.collection("workflow-events").find().toArray();
+  res.json({ events: allEvents });
+});
+
+router.post("/", async (req, res) =>{
+  const eventData = req.body;
+  const result = await db.collection("workflow-events").insertOne({...eventData});
+
+  res.status(201).json({
+    message: "Event created.",
+    event: { ...eventData, id: result.insertedId }
+  });
+});
+
+export default router;
