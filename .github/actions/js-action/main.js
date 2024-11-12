7210f5ea5e5811cfa7f92a8259e02eb0c0c2b732
yairyakobson <yairnet92@gmail.com>
2024-11-12 14:54:25 +0200

    add: added aws s3 javascript logic

--- .github/actions/js-action/main.js
+++ .github/actions/js-action/main.js
@@ -3,6 +3,15 @@ import exec from "@actions/exec";
 import github from "@actions/github";
 
 function run(){
+  // Input values from the js action file
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+  
+  // Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  
   core.notice("I was executed");
 }
 run();
